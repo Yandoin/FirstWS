16cf4470fb4f93059218ca8a3bcdf101bfdc06d8
Yandoin <yannick@doing.ch>
2019-06-18 15:02:46 +0200

    First Commit

---
+++ First Project/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int x = 50000;
+
+        for (int i = 0; i < x; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
